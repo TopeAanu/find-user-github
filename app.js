a86befde4ee4735066731e36022c3df6e4abf777
TopeAanu <topefasasi99@gmail.com>
2023-10-31 00:02:45 +0100

    update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ searchInput.addEventListener('keyup', (e) => {
     github.getUser(userValue).then(data => {
       if(data.profile.message === 'Not Found') {
         // show alert
-        ui.showAlert('User not found', 'alert');
+        ui.showAlert('User not found', 'alert alert-danger');
       } else {
         // show profile
         ui.showProfile(data.profile);
--- ui.js
+++ ui.js
@@ -6,13 +6,13 @@ class UI {
   // display profile in ui 
   showProfile(user) {
     this.profile.innerHTML = `
-      <div class="card card-body mb-3">
-        <div class="row">
-          <div class="col-md-3">
-            <img class="img-fluid mb-2" src="${user.avatar_url}">
-            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+      <div class="card">
+        <div class="row row2">
+          <div class="col-1">
+            <img class="img" src="${user.avatar_url}">
+            <a href="${user.html_url}" target="_blank" class="btn">View Profile</a>
           </div>
-          <div class="col-md-9">
+          <div class="col-2">
             <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
             <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
             <span class="badge badge-success">Followers: ${user.followers}</span>
@@ -21,13 +21,13 @@ class UI {
             <ul class="list-group">
               <li class="list-group-item">Company: ${user.company}</li>
               <li class="list-group-item">Website/Blog: ${user.blog}</li>
-              <li class="list-group-item">Company: ${user.location}</li>
-              <li class="list-group-item">Company: ${user.created_at}</li>
+              <li class="list-group-item">Location: ${user.location}</li>
+              <li class="list-group-item">Created: ${user.created_at}</li>
             <ul>
           </div>
         </div>
       </div>
-      <h3 class="page-heading mb-3">Latest Repos</h3>
+      <h3 class="heading">Latest Repos</h3>
       <div id="repos"</div>
     `;
   }
@@ -38,16 +38,16 @@ class UI {
 
     repos.forEach(function(repo) {
       output += `
-        <div class="card card-body mb-2">
+        <div class="card">
           <div class="row">
-          <div class="col-md-6">
-            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-          </div>
-          <div class="col-md-6">
-            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
-            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
-            <span class="badge badge-success">Forks: ${repo.forks_count}</span>
-          </div>
+            <div class="col-3">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-3">
+              <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+              <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+              <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+            </div>
           </div>
         </div>
       `;
@@ -72,17 +72,31 @@ class UI {
     div.appendChild(document.createTextNode(message));
 
     // get parent 
-    const container = document.querySelector('.search-con');
+    const container = document.querySelector('.searchCon');
 
     // get search box
     const search = document.querySelector('.search');
 
-    // insert alertg
+    // insert alert
     container.insertBefore(div, search);
 
     // timeout after 3 secs
     setTimeout(() => {
       this.clearAlert();
     }, 3000);
+  }
+
+  // clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  // clear profile
+  clearProfile() {
+    this.profile.innerHTML = '';
   }
 }
